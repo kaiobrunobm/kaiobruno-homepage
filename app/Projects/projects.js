d291c39bc11a71447b2049746b93e89c7596cf4d
Kaio Bruno Barbosa Magalhães <bmkaiobruno@gmail.com>
2023-07-12 17:43:12 -0300

    Getting a job

--- app/Projects/projects.js
+++ app/Projects/projects.js
@@ -1,54 +1,11 @@
 export const projects = [
   {
     id: 1,
-    name: "Mobills Clone",
-    description: "Mobills is a website and app that take care of your finance",
-    logoUrl: "/logos/mobills-md-logo.svg",
-    htmlUrl: "https://mobills-clone.vercel.app/",
-    repoUrl: "https://github.com/kaiobrunobm/mobills-clone",
-  },
-  {
-    id: 2,
-    name: "React.",
-    description:
-      "Monitor your data analytics to increase revenue for BTB, BTC, & SASS platforms.",
-    logoUrl: "",
-    htmlUrl: "https://full-app-tutorial.vercel.app/",
-    repoUrl: "https://github.com/kaiobrunobm/full-app-tutorial",
-  },
-  {
-    id: 3,
-    name: "Card details form",
-    description:
-      "Interactive from with card card details by Front end mentors.",
-    logoUrl: "",
-    htmlUrl: "https://interactive-card-details-form-bay.vercel.app/",
-    repoUrl: "https://github.com/kaiobrunobm/interactive-card-details-form",
-  },
-  {
-    id: 4,
-    name: "Intro section",
-    description: "Intro web page static with dropdown by Front end mentors",
-    logoUrl: "",
-    htmlUrl: "https://intro-section-with-dropdown-b21e.vercel.app/",
-    repoUrl: "https://github.com/kaiobrunobm/intro-section-with-dropdown",
-  },
-  {
-    id: 5,
     name: "Portifolio 1.0",
     description:
       "My first portifolio made on my beginning in the web development",
     logoUrl: "",
     htmlUrl: "https://kaiobrunobm.github.io/portfolio/",
     repoUrl: "https://github.com/kaiobrunobm/portfolio",
-  },
-  {
-    id: 6,
-    name: "IOS calculator clone",
-    description:
-      "a simple calculator made with javascript coping the IOS Calculator design",
-    logoUrl: "",
-    htmlUrl: "https://kaiobrunobm.github.io/ios-calculator/",
-    repoUrl: "https://github.com/kaiobrunobm/ios-calculator",
   },
 ];
